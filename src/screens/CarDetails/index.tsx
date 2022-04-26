1eb142e561158e573bac34b087d04c9552b94097
Miranda <omirandavictor@gmail.com>
2022-04-26 07:49:23 -0300

    Accessory and button Component is - DONE!

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { BackButton } from '../../components/BackButton';
+
+import { Accessory } from '../../components/Accessory';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Button } from '../../components/Button';
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
 
 import {
   Container,
@@ -14,7 +23,11 @@ import {
   Rent,
   Period,
   Price,
+  About,
+  Accessories,
+  Footer
 } from './styles';
+import { BackButton } from '../../components/BackButton';
 
 export function CarDetails(){
   return (
@@ -41,7 +54,27 @@ export function CarDetails(){
             <Price>R$ 260</Price>
           </Rent>
         </Details>
+
+        <Accessories>
+          <Accessory name="322" icon={speedSvg}/>
+          <Accessory name="2.4s" icon={accelerationSvg}/>
+          <Accessory name="1,020 HP" icon={forceSvg}/>
+          <Accessory name="Electrical" icon={gasolineSvg}/>
+          <Accessory name="Auto" icon={exchangeSvg}/>
+          <Accessory name="4 Pessoas" icon={peopleSvg}/>
+        </Accessories>
+
+        <About>
+          Ele é um dos veículos mais vendidos na gama da marca.
+          Esse sedã tem dois motores elétricos, um em cada eixo,
+          e tração integral. Pode alcançar 96 km/h em 2,4 segundos
+          na versão Performance
+        </About>
       </Content>
+
+      <Footer>
+        <Button title="Confirmar" color=""/>
+      </Footer>
 
     </Container>
   );
