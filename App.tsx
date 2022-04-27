bd05fc68df83106de06269798f2b4e18b0cf785d
Miranda <omirandavictor@gmail.com>
2022-04-27 06:48:08 -0300

    Feedback of Scheduling complete - DONE

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import AppLoading from 'expo-app-loading'
 import React from 'react'
 import { ThemeProvider } from 'styled-components/native'
 
-import { SchedulingDetails } from './src/screens/SchedulingDetails'
+import { SchedulingComplete } from './src/screens/SchedulingComplete'
 import theme from './src/styles/theme'
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   )
 }
