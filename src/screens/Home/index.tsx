1c5b7ed7529566f551c660325c6f6f8e2014c0cd
Miranda <omirandavictor@gmail.com>
2022-05-03 20:59:19 -0300

    Total available cars at home screen - done

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -69,7 +69,7 @@ export function Home(){
           />
 
           <TotalCars>
-            total 12 carros
+            total {cars.length} carros
         </TotalCars>
         </HeaderContent>
       </Header>
