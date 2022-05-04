f5ead74a5e3134d325ed03daf8791e7ffd4376da
Miranda <omirandavictor@gmail.com>
2022-05-04 06:55:50 -0300

    Animation of imageSlider - DOING

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import {
   Container,
@@ -12,22 +13,46 @@ interface Props {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: Props){
+  const [imageIndex, setImageIndex] = useState(0);
+
+  function indexChanged(info: ChangeImageProps){
+    setImageIndex(info.viewableItems[0])
+  }
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {
+          imagesUrl.map((_, index) =>( // O map recebe 2 propriedades, o item e o index, quando não queremos utilizar o item, basta passar o underline para que ele seja ignorado
+            <ImageIndex
+              key={String(index)}
+              active={true}
+            />
+          ))
+        }
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage
-          source={{uri: imagesUrl[0]}}
-          resizeMode="contain"
+        <FlatList
+          data={imagesUrl}
+          keyExtractor={key => key}
+          renderItem={({item}) => (
+            <CarImageWrapper>
+              <CarImage
+                source={{uri: item}}
+                resizeMode="contain"
+              />
+            </CarImageWrapper>
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          /* onViewableItemsChanged={indexChanged} */
         />
-      </CarImageWrapper>
     </Container>
   );
 }
