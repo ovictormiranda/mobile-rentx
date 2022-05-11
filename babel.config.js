34ce759b77837bae7116341fd26539063f782919
Miranda <omirandavictor@gmail.com>
2022-05-11 20:25:26 -0300

    Configs of WatermelonDB

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      ["@babel/plugin-proposal-decorators", { "legacy": true }]
+    ],
   }
 }
